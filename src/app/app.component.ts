0fe2715190365f5f0457388268998a091d7fd6a2
Punit Sehrawat <punit.sehrawat@nagarro.com>
2018-11-12 10:21:39 +0530

    changesAdded

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { appService } from './app.service';
+import { User } from './user.model';
 
 @Component({
   selector: 'pm-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [appService]
 })
-export class AppComponent {
-  title = 'Angular: Getting Started';
+export class AppComponent implements OnInit {
+  title = 'testing';
+
+   users: User[];
+
+  constructor(private appService: appService) {
+
+  }
+
+  ngOnInit() {
+    this.appService.getUsers()
+      .subscribe( data => {
+        this.users = data;
+        //console.log(this.users);
+      });
+  };
 }
---
+++ src/app/app.service.ts
@@ -0,0 +1,21 @@
+import { 
+    Injectable 
+ } from '@angular/core';  
+ 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { User } from './user.model';
+
+const httpOptions = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  };
+
+ @Injectable()
+ export class appService {  
+
+    constructor(private http:HttpClient) {}
+    private userUrl = 'http://127.0.0.1:8080/demo/all';
+
+    getUsers(){
+        return this.http.get<User[]>(this.userUrl);
+    }
+ } 
